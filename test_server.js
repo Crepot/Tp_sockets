84a2c8b8589e02de109915409eb4b6a8fdceb8f5
Crepot <fjgallardo97@gmail.com>
2020-11-02 21:12:18 -0300

    First commit

---
+++ test_server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',function(socket){
+    console.log('a user is connected with socket');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+server.listen(8080,function(){
+    console.log('server listening on Localhost port 8080');
+});
